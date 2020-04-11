6407f4e3f71c765c0edf312ce5c04e44b4431bcd
QuintonEL <quintonlaborde@gmail.com>
2020-04-11 13:44:45 -0600

    added in require for cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
